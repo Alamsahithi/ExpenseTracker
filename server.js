b1ecb7da153cf65e8e51c94cf284d900dcd50c18
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-07-16 20:36:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,72 +1,85 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
 
-// Create MySQL connection
+const app = express();
+const port = 3000;
+
+// MySQL configuration
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'SriSahi@22',
-  database: 'expensetracker'
+  host: "localhost",
+  user: "root",
+  password: "SriSahi@22",
+  database: "expensetracker",
 });
 
-// Connect to MySQL
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL database');
+    console.error("Error connecting to the database: ", err);
+    return;
   }
-});
+  console.log("Connected to the database");
 
-// Create users table if it doesn't exist
-const createUsersTable = `
-  CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-  )
-`;
+  // Create the users table if it doesn't exist
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      email VARCHAR(255) NOT NULL,
+      password VARCHAR(255) NOT NULL
+    )
+  `;
 
-connection.query(createUsersTable, (err) => {
-  if (err) {
-    console.error('Error creating users table:', err);
-  } else {
-    console.log('Users table created or already exists');
-  }
-});
-
-// Create Express app
-const app = express();
-
-// Middleware to parse request bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Enable CORS
-app.use(cors());
-
-// Handle POST request to /signup
-app.post('/signup', (req, res) => {
-  const { name, email, password } = req.body;
-
-  // Perform validation here if needed
-
-  // Insert the user into the database
-  const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
-  connection.query(query, [name, email, password], (err, results) => {
+  connection.query(createTableQuery, (err, results) => {
     if (err) {
-      console.error('Error creating user:', err);
-      res.status(500).json({ error: 'Failed to create user' });
+      console.error("Error creating users table: ", err);
     } else {
-      res.status(200).json({ message: 'User created successfully' });
+      console.log("Users table created or already exists");
     }
   });
 });
 
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Signup route
+app.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = {
+    email: email,
+    password: password,
+  };
+
+  connection.query(
+    "SELECT * FROM users WHERE email = ?",
+    email,
+    (err, results) => {
+      if (err) {
+        console.error("Error checking email uniqueness: ", err);
+        res.status(500).json({ error: "Failed to check email uniqueness" });
+        return;
+      }
+
+      if (results.length > 0) {
+        // Email already exists
+        res.status(409).json({ error: "Email already exists" });
+      } else {
+        // Email is unique, store user details
+        connection.query("INSERT INTO users SET ?", user, (err, results) => {
+          if (err) {
+            console.error("Error storing user details: ", err);
+            res.status(500).json({ error: "Failed to store user details" });
+            return;
+          }
+          res.status(200).json({ message: "User details stored successfully" });
+        });
+      }
+    }
+  );
+});
+
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
