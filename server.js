bf070c8b86577d062d7266e994e7d4fa408775d9
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-07-17 13:00:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3000;
@@ -48,39 +49,50 @@ app.use(cors());
 app.post("/signup", (req, res) => {
   const { name, email, phone, password } = req.body;
 
-  const user = {
-    name: name,
-    email: email,
-    phone: phone,
-    password: password,
-  };
-
-  connection.query(
-    "SELECT * FROM users WHERE email = ?",
-    email,
-    (err, results) => {
-      if (err) {
-        console.error("Error checking email uniqueness: ", err);
-        res.status(500).json({ error: "Failed to check email uniqueness" });
-        return;
-      }
-
-      if (results.length > 0) {
-        // Email already exists
-        res.status(409).json({ error: "Email already exists" });
-      } else {
-        // Email is unique, store user details
-        connection.query("INSERT INTO users SET ?", user, (err, results) => {
-          if (err) {
-            console.error("Error storing user details: ", err);
-            res.status(500).json({ error: "Failed to store user details" });
-            return;
-          }
-          res.status(200).json({ message: "User details stored successfully" });
-        });
-      }
+  // Generate a salt and hash the password
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      console.error("Error hashing password: ", err);
+      res.status(500).json({ error: "Failed to hash password" });
+      return;
     }
-  );
+
+    const user = {
+      name: name,
+      email: email,
+      phone: phone,
+      password: hashedPassword,
+    };
+
+    connection.query(
+      "SELECT * FROM users WHERE email = ?",
+      email,
+      (err, results) => {
+        if (err) {
+          console.error("Error checking email: ", err);
+          res.status(500).json({ error: "Failed to check email" });
+          return;
+        }
+
+        if (results.length === 0) {
+          // Email does not exist
+          connection.query("INSERT INTO users SET ?", user, (err, results) => {
+            if (err) {
+              console.error("Error storing user details: ", err);
+              res.status(500).json({ error: "Failed to store user details" });
+              return;
+            }
+            res
+              .status(200)
+              .json({ message: "User details stored successfully" });
+          });
+        } else {
+          // Email already exists
+          res.status(409).json({ error: "Email already exists" });
+        }
+      }
+    );
+  });
 });
 
 // Login route
@@ -103,13 +115,22 @@ app.post("/login", (req, res) => {
       } else {
         const user = results[0];
 
-        if (user.password === password) {
-          // Password matches
-          res.status(200).json({ message: "User logged in successfully" });
-        } else {
-          // Password does not match
-          res.status(401).json({ error: "Email and password do not match" });
-        }
+        // Compare the login password with the stored hashed password
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) {
+            console.error("Error comparing passwords: ", err);
+            res.status(500).json({ error: "Failed to compare passwords" });
+            return;
+          }
+
+          if (isMatch) {
+            // Password matches
+            res.status(200).json({ message: "User logged in successfully" });
+          } else {
+            // Password does not match
+            res.status(401).json({ error: "Email and password do not match" });
+          }
+        });
       }
     }
   );
