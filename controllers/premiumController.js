776b84f9c5152f75ec2774727a2e59a069c04a3e
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-07-27 19:41:49 +0530

    optimizing leaderboard

--- controllers/premiumController.js
+++ controllers/premiumController.js
@@ -1,5 +1,6 @@
 const Expense = require("../models/expenseModel");
 const User = require("../models/userModel");
+const Sequelize = require("sequelize");
 
 const getLeaderBorad = async (req, res) => {
   try {
@@ -10,35 +11,23 @@ const getLeaderBorad = async (req, res) => {
     if (!user.premiumUser) {
       return res.status(400).json({ message: "You are not a premium user" });
     }
-    const expenses = await Expense.findAll();
-    if (!expenses) {
+    const aggregatedExpenses = await User.findAll({
+      attributes: [
+        "id",
+        "fullName",
+        [
+          Sequelize.fn("sum", Sequelize.col("Expenses.amount")),
+          "totalExpenses",
+        ],
+      ],
+      include: [{ model: Expense, attributes: [] }],
+      group: ["User.id"],
+      order: [[Sequelize.col("totalExpenses"), "DESC"]],
+    });
+    if (!aggregatedExpenses) {
       return res.status(500).json({ message: "Internal server error" });
     }
-    const leaderBoard = {};
-    for (const expense of expenses) {
-      const userId = expense?.UserId;
-      if (!leaderBoard[userId]) {
-        leaderBoard[userId] = expense?.amount;
-      } else {
-        leaderBoard[userId] += expense?.amount;
-      }
-    }
-    const sortedLeaderBoard = Object.entries(leaderBoard).map(
-      ([userId, totalExpenses]) => ({
-        userId,
-        totalExpenses,
-      })
-    );
-    const users = await User.findAll();
-    const userIdToUsernameMap = {};
-    for (const user of users) {
-      userIdToUsernameMap[user.id] = user.fullName;
-    }
-    for (const userTotalExpense of sortedLeaderBoard) {
-      userTotalExpense.username = userIdToUsernameMap[userTotalExpense.userId];
-    }
-    sortedLeaderBoard.sort((a, b) => b.totalExpenses - a.totalExpenses);
-    return res.status(200).json(sortedLeaderBoard);
+    return res.status(200).json(aggregatedExpenses);
   } catch (error) {
     console.error("Error in getLeaderBorad:", error);
     return res.status(500).json({ message: "Internal server error" });
