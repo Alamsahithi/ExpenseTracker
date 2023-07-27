5e351cb2af54cb4976554038f80b5162b2115224
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-07-27 20:57:49 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,7 @@ const User = sequelize.define("User", {
   phone: { type: DataTypes.STRING(200), allowNull: false },
   password: { type: DataTypes.STRING(200), allowNull: false },
   premiumUser: { type: DataTypes.BOOLEAN, allowNull: false },
+  totalExpenses: { type: DataTypes.INTEGER, allowNull: false },
 });
 
 User.prototype.toJSON = function () {
