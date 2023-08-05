ce6867096603ec43d1c0c4467d8826bb26084b70
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-08-05 19:59:30 +0530

    working on forgot password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,67 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const { v4: uuidv4 } = require("uuid");
+const ForgotPasswordRequests = require("../models/forgotPasswordRequestsModel");
+const fs = require("fs");
+const path = require("path");
+const sequelize = require("../configs/databaseConfig");
+const AWS = require("aws-sdk");
+require("dotenv").config();
+
+const uploadToS3 = (data, fileName) => {
+  const BUCKET_NAME = "srinathexpensetrackingapp123";
+  const USER_KEY = "AKIAX6DN4NXJRH5AWXTK";
+  const USER_SECRET = "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";
+  let s3Bucket = new AWS.S3({
+    accessKeyId: USER_KEY,
+    secretAccessKey: USER_SECRET,
+  });
+  var params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName,
+    Body: data,
+    ACL: "public-read",
+  };
+  return new Promise((resolve, reject) => {
+    s3Bucket.upload(params, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res?.Location);
+      }
+    });
+  });
+};
+
+const download = async (req, res) => {
+  try {
+    const user = await User.findByPk(req?.user?.id);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    if (!user.premiumUser) {
+      return res.status(400).json({ message: "You are not a premium user" });
+    }
+    const expenses = await user.getExpenses();
+    if (!expenses) {
+      return res
+        .status(400)
+        .json({ message: "Something went wrong, please try again" });
+    }
+    if (expenses?.length === 0) {
+      return res.status(200).json({ message: "You don't have any expenses" });
+    }
+    const stringifiesExpenses = JSON.stringify(expenses);
+
+    const fileName = `Expenses${user.id}/${new Date()}.txt`;
+    const fileUrl = await uploadToS3(stringifiesExpenses, fileName);
+    return res.status(200).json({ fileUrl });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 const generateToken = ({ id, fullName, email, phone, premiumUser }) => {
   return jwt.sign(
@@ -162,9 +223,139 @@ const editUserProfile = async (req, res) => {
   }
 };
 
+//forgot password
+const forgotpassword = async (req, res) => {
+  const { forgotPasswordEmail } = req.body;
+  try {
+    if (!forgotPasswordEmail) {
+      return res.status(500).json({ message: "Please send a valid email id" });
+    }
+    const user = await User.findOne({ where: { email: forgotPasswordEmail } });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Please enter the registered email id" });
+    }
+    const requestId = uuidv4();
+    const forgotPasswordRequest = await ForgotPasswordRequests.create({
+      id: requestId,
+      userId: user.id,
+      isactive: true,
+    });
+    if (!forgotPasswordRequest) {
+      return res
+        .status(400)
+        .json({ message: "Something went wrong please try again" });
+    }
+    const resetUrl = `http://localhost:8080/user/resetpassword/${requestId}`;
+    const client = SibApiV3Sdk.ApiClient.instance;
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey =
+      "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";
+    const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+    const sender = { email: "srinath3898@gmail.com" };
+    const recievers = [{ email: forgotPasswordEmail }];
+    tranEmailApi
+      .sendTransacEmail({
+        sender,
+        to: recievers,
+        subject: "Reset Password",
+        htmlContent: `<p>Hello ${user.fullName},</p><p>Click <a href="${resetUrl}">here</a> to reset your password.</p>`,
+      })
+      .then(() => {
+        return res.status(201).json({
+          message:
+            "An email has been sent to your registered mail id with the reset password link",
+        });
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: error });
+      });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      data: null,
+      message: error.message,
+    });
+  }
+};
+
+const resetpassword = async (req, res) => {
+  const { requestId } = req.params;
+  try {
+    const forgotPasswordRequest = await ForgotPasswordRequests.findOne({
+      where: { id: requestId, isactive: true },
+    });
+    if (!forgotPasswordRequest) {
+      return res.status(400).json({ message: "Invalid or expired reset link" });
+    }
+    const resetFormHTML = fs.readFileSync(
+      path.join(__dirname, "..", "resetPasswordForm.html"),
+      "utf8"
+    );
+    const formWithRequestId = resetFormHTML.replace("{{requestId}}", requestId);
+    return res.send(formWithRequestId);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const updatepassword = async (req, res) => {
+  const { newPassword, confirmPassword, requestId } = req.body;
+  const transaction = await sequelize.transaction();
+  try {
+    if (newPassword !== confirmPassword) {
+      await transaction.rollback();
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+    const forgotPasswordRequest = await ForgotPasswordRequests.findOne({
+      where: { id: requestId, isactive: true },
+    });
+    if (!forgotPasswordRequest) {
+      return res.status(400).json({ message: "Invalid or expired reset link" });
+    }
+    const user = await User.findOne({
+      where: { id: forgotPasswordRequest.userId },
+    });
+    if (!user) {
+      await transaction.rollback();
+      return res.status(404).json({ message: "User not found" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    const updatedUser = await User.update(
+      { password: hashedPassword },
+      { where: { id: user.id } }
+    );
+    if (!updatedUser) {
+      await transaction.rollback();
+      return res
+        .status(404)
+        .json({ message: "Something went wrong please try again" });
+    }
+    const updatedForgotPasswordRequest = await forgotPasswordRequest.update({
+      isactive: false,
+    });
+    if (!updatedForgotPasswordRequest) {
+      await transaction.rollback();
+      return res
+        .status(404)
+        .json({ message: "Something went wrong please try again" });
+    }
+    await transaction.commit();
+    return res.status(200).json({ message: "Password reset successful" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   userSignup,
   userSignin,
   getUserProfile,
   editUserProfile,
+  forgotpassword,
+  resetpassword,
+  updatepassword,
+  download,
 };
