154d33e8fec507f82ab9f3a4eaa0b1c216a29aed
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-08-05 20:01:13 +0530

    working on forgot password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,10 @@ const {
   userSignin,
   getUserProfile,
   editUserProfile,
+  forgotpassword,
+  resetpassword,
+  updatepassword,
+  download,
 } = require("../controllers/userController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -15,5 +19,13 @@ router.post("/signin", userSignin);
 router.get("/profile", protect, getUserProfile);
 
 router.post("/editProfile", protect, editUserProfile);
+
+router.post("/forgotpassword", forgotpassword);
+
+router.get("/resetpassword/:requestId", resetpassword);
+
+router.post("/updatepassword", updatepassword);
+
+router.get("/download", protect, download);
 
 module.exports = router;
