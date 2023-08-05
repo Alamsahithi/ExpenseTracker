dbe1b7d345769849ca643f3e0d095c3cff2ae1ca
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-08-05 20:24:37 +0530

    userexpense download

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,5 +1,7 @@
+const sequelize = require("../configs/databaseConfig");
 const Expense = require("../models/expenseModel");
 const User = require("../models/userModel");
+const { Op } = require("sequelize");
 
 const getAllExpenses = async (req, res) => {
   try {
@@ -21,29 +23,124 @@ const getAllExpenses = async (req, res) => {
   }
 };
 
-const addExpense = async (req, res) => {
-  const { amount, category, description } = req.body;
+const dailyExpenses = async (req, res) => {
   try {
     const user = await User.findByPk(req.user.id);
-    if (user) {
-      const expense = await user.createExpense({
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    if (!user.premiumUser) {
+      return res.status(400).json({ message: "You are not a premium user" });
+    }
+    const currentDate = new Date();
+    const startOfDay = new Date(currentDate);
+    startOfDay.setHours(0, 0, 0, 0);
+    const endOfDay = new Date(currentDate);
+    endOfDay.setHours(23, 59, 59, 999);
+    const expenses = await Expense.findAll({
+      where: {
+        UserId: user.id,
+        createdAt: {
+          [Op.between]: [startOfDay, endOfDay],
+        },
+      },
+    });
+
+    return res.status(200).json(expenses);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: false, data: null, message: error.message });
+  }
+};
+
+const weeklyExpenses = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    if (!user.premiumUser) {
+      return res.status(400).json({ message: "You are not a premium user" });
+    }
+    const currentDate = new Date();
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(currentDate.getDate() - 7);
+    sevenDaysAgo.setHours(0, 0, 0, 0);
+    currentDate.setHours(23, 59, 59, 999);
+    const expenses = await Expense.findAll({
+      where: {
+        UserId: user.id,
+        createdAt: {
+          [Op.between]: [sevenDaysAgo, currentDate],
+        },
+      },
+    });
+    return res.status(200).json(expenses);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: false, data: null, message: error.message });
+  }
+};
+
+const monthlyExpenses = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    if (!user.premiumUser) {
+      return res.status(400).json({ message: "You are not a premium user" });
+    }
+    const currentDate = new Date();
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(currentDate.getDate() - 30);
+    thirtyDaysAgo.setHours(0, 0, 0, 0);
+    currentDate.setHours(23, 59, 59, 999);
+    const expenses = await Expense.findAll({
+      where: {
+        UserId: user.id,
+        createdAt: {
+          [Op.between]: [thirtyDaysAgo, currentDate],
+        },
+      },
+    });
+
+    return res.status(200).json(expenses);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const addExpense = async (req, res) => {
+  const { amount, category, description } = req.body;
+  const transaction = await sequelize.transaction();
+  try {
+    const user = await User.findByPk(req.user.id);
+    if (!user) {
+      await transaction.rollback();
+      return res.status(400).json({ message: "User not found" });
+    }
+    const updatedTotalExpenses = user.totalExpenses + parseFloat(amount);
+    await user.update({ totalExpenses: updatedTotalExpenses }, { transaction });
+    const expense = await user.createExpense(
+      {
         amount: parseFloat(amount),
         category,
         description,
-      });
-      if (expense) {
-        const updatedTotalExpenses = user.totalExpenses + parseFloat(amount);
-        await user.update({ totalExpenses: updatedTotalExpenses });
-        return res.status(201).json({ message: "Expense saved successfully" });
-      } else {
-        return res
-          .status(500)
-          .json({ error: "Failed to store expense details" });
-      }
-    } else {
-      return res.status(400).json({ message: "User not found" });
+      },
+      { transaction }
+    );
+
+    if (!expense) {
+      await transaction.rollback();
+      return res.status(500).json({ error: "Failed to store expense details" });
     }
+    await transaction.commit();
+    return res.status(201).json({ message: "Expense saved successfully" });
   } catch (error) {
+    await transaction.rollback();
     return res
       .status(500)
       .json({ status: false, data: null, message: error.message });
@@ -80,4 +177,11 @@ const deleteExpense = async (req, res) => {
   }
 };
 
-module.exports = { getAllExpenses, addExpense, deleteExpense };
+module.exports = {
+  getAllExpenses,
+  addExpense,
+  deleteExpense,
+  dailyExpenses,
+  weeklyExpenses,
+  monthlyExpenses,
+};
